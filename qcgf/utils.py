12ce8d5e1f3fc2194ace9cea07b628f43bf66e48
Junjie Yang <yangjunjie0320@gmail.com>
2023-07-26 00:12:04 -0700

    added comments

--- qcgf/utils.py
+++ qcgf/utils.py
@@ -1,19 +1,24 @@
 import inspect
-
 from pyscf import fci
 
-def make_h1_and_h2(mol_obj=None, coeff_mo=None):
-    fci_obj  = fci.FCI(mol_obj, mo=coeff_mo, singlet=False)
+
+def make_h1_and_h2(molecule=None, coefficients=None):
+    fci_object = fci.FCI(molecule, mo=coefficients, singlet=False)
     
-    kwargs = inspect.signature(fci_obj.kernel).parameters
-    h1e    = kwargs.get('h1e'  ).default
-    h2e    = kwargs.get('eri'  ).default
-
-    norb   = kwargs.get('norb').default
-    norb2  = norb * (norb + 1) // 2
-    nelec  = kwargs.get('nelec').default
-
-    assert h1e.shape == (norb, norb)
-    assert h2e.shape == (norb2, norb2)
-
-    return nelec, norb, h1e, h2e
+    # Get the parameters of the fci.kernel function
+    kwargs = inspect.signature(fci_object.kernel).parameters
+    
+    # Get the default values of h1e and h2e
+    h1e_default = kwargs.get('h1e').default
+    h2e_default = kwargs.get('eri').default
+    
+    # Get the default values of norb and nelec from the FCI object
+    norb = kwargs.get('norb').default
+    norb2 = norb * (norb + 1) // 2
+    nelec = kwargs.get('nelec').default
+    
+    # Check the shapes of h1e and h2e
+    assert h1e_default.shape == (norb, norb)
+    assert h2e_default.shape == (norb2, norb2)
+    
+    return nelec, norb, h1e_default, h2e_default
